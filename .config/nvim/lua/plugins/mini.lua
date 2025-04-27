5f875ca529f88c96164333b0f7095a28802c818e
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-04-27 23:34:58 +0100

    feat: enhance mini config
    - Add mini.clue
    - Config mini.files to not open automatically when neovim starts

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -35,7 +35,11 @@ return {
         },
       })
       vim.api.nvim_command("highlight StatusLineRecording guifg=#ff0000 guibg=NONE gui=bold")
-      require("mini.files").setup()
+      require("mini.files").setup({
+        options = {
+          use_as_default_explorer = false, -- avoid it to open automaically when neovim starts
+        },
+      })
       require("mini.diff").setup()
       require("mini.pick").setup({
         options = {
@@ -44,6 +48,53 @@ return {
         mappings = {
           move_up = "<C-k>",
           move_down = "<C-j>",
+        },
+      })
+      local miniclue = require("mini.clue")
+      miniclue.setup({
+        triggers = {
+          -- Leader triggers
+          { mode = "n", keys = "<Leader>" },
+          { mode = "x", keys = "<Leader>" },
+
+          -- Built-in completion
+          { mode = "i", keys = "<C-x>" },
+
+          -- `g` key
+          { mode = "n", keys = "g" },
+          { mode = "x", keys = "g" },
+
+          -- Marks
+          { mode = "n", keys = "'" },
+          { mode = "n", keys = "`" },
+          { mode = "x", keys = "'" },
+          { mode = "x", keys = "`" },
+
+          -- Registers
+          { mode = "n", keys = '"' },
+          { mode = "x", keys = '"' },
+          { mode = "i", keys = "<C-r>" },
+          { mode = "c", keys = "<C-r>" },
+
+          -- Window commands
+          { mode = "n", keys = "<C-w>" },
+
+          -- `z` key
+          { mode = "n", keys = "z" },
+          { mode = "x", keys = "z" },
+        },
+
+        clues = {
+          -- Enhance this by adding descriptions for <Leader> mapping groups
+          miniclue.gen_clues.builtin_completion(),
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+        },
+        window = {
+          delay = 300,
         },
       })
     end,
