24ddee964d872280745899f8bd47941d7d0b2811
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-04-27 19:46:51 +0100

    feat: add lazygit and diffview

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gf", "<cmd>DiffviewFileHistory<cr>", desc = "File history" },
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diff view" },
+    },
+  },
+}
