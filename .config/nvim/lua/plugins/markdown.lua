e85f9277ac7ea2e4e0613fcb1395d032a2b5f91a
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-04-28 03:26:52 +0100

    feat: enhance markdown checkboxes config

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -3,10 +3,27 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "echasnovski/mini.nvim",
-    "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {},
+  checkbox = {
+    enabled = false,
+    render_modes = false,
+    right_pad = 10,
+    unchecked = {
+      icon = "󰄱 ",
+      highlight = "RenderMarkdownUnchecked",
+      scope_highlight = nil,
+    },
+    checked = {
+      icon = "󰱒",
+      highlight = "RenderMarkdownChecked",
+      scope_highlight = nil,
+    },
+    custom = {
+      todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+    },
+  },
 }
