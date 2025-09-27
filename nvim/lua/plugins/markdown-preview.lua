2a889042ca301d63a90374a24bd14a0596507399
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-09-27 22:07:03 +0100

    chore: update ghostty + nvim plugins

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,41 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = "cd app && npm install",
+  keys = {
+    {
+      "<leader>mp",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Markdown Preview",
+    },
+  },
+  config = function()
+    vim.g.mkdp_auto_start = 0
+    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_refresh_slow = 0
+    vim.g.mkdp_command_for_global = 0
+    vim.g.mkdp_open_to_the_world = 0
+    vim.g.mkdp_open_ip = ""
+    vim.g.mkdp_browser = ""
+    vim.g.mkdp_echo_preview_url = 1
+    vim.g.mkdp_browserfunc = ""
+    vim.g.mkdp_theme = "dark"
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_page_title = "「${name}」"
+    vim.g.mkdp_preview_options = {
+      mkit = {},
+      katex = {},
+      uml = {},
+      maid = {},
+      disable_sync_scroll = 0,
+      sync_scroll_type = "middle",
+      hide_yaml_meta = 1,
+      sequence_diagrams = {},
+      flowchart_diagrams = {},
+      content_editable = false,
+      disable_filename = 0,
+      toc = {},
+    }
+  end,
+}
