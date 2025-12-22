8e0f5cafc1496abe031e1bc2cd929cae892a17ca
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-12-22 16:47:16 +0000

    chore: add nvim themes

---
+++ macos/nvim/lua/plugins/apprentice.lua
@@ -0,0 +1,7 @@
+return {
+  "adisen99/apprentice.nvim",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+  dependencies = { "rktjmp/lush.nvim" },
+}
---
+++ macos/nvim/lua/plugins/blue-moon.lua
@@ -0,0 +1,6 @@
+return {
+  "kyazdani42/blue-moon",
+  enabled = true,
+  lazy = true,
+  priority = 1000,
+}
