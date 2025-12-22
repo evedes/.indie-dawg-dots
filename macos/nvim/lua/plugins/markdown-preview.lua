eef2e850409ad1ebe2347aae59b271d1dcf2b1cb
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-12-22 17:23:49 +0000

    fix: markdown preview lazy sync

--- macos/nvim/lua/plugins/markdown-preview.lua
+++ macos/nvim/lua/plugins/markdown-preview.lua
@@ -2,7 +2,7 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = "markdown",
-  build = "cd app && npm install",
+  build = ":call mkdp#util#install()",
   keys = {
     { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview" },
   },
