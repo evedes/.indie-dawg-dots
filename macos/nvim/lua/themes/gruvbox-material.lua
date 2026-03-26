d585b0cd81aad495179f89d0e4588e7382048866
Eduardo Vedes <eduardo.vedes@gmail.com>
2026-03-26 21:52:24 +0000

    chore: update neovim

--- macos/nvim/lua/themes/gruvbox-material.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "sainnhe/gruvbox-material",
-  enabled = true,
-  lazy = true,
-  priority = 1000,
-}
--- macos/nvim/lua/themes/nordic.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "AlexvZyl/nordic.nvim",
-  enabled = true,
-  lazy = true,
-  priority = 1000,
-}
