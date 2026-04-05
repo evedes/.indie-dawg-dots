2a1eb642746ecb45170e30f51ef4cfd3e0b8d58d
Eduardo Vedes <eduardo.vedes@gmail.com>
2026-04-05 18:23:14 +0100

    chore: add newnvim config for macos

---
+++ macos/newnvim/after/ftplugin/typescript.lua
@@ -0,0 +1,8 @@
+local set = vim.opt_local
+
+-- Match common TypeScript formatting conventions
+set.tabstop = 2
+set.shiftwidth = 2
+set.expandtab = true
+set.textwidth = 120
+set.smartindent = true
