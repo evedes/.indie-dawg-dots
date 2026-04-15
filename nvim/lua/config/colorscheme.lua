372eebaa72e1ebb6e42c3802389143199413bf6b
Eduardo Vedes <eduardo.vedes@gmail.com>
2026-04-15 18:30:11 +0100

    chore: update nvim config

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,5 +1,9 @@
 vim.pack.add {
 	"https://github.com/rebelot/kanagawa.nvim",
+  "https://github.com/catppuccin/nvim"
 }
 
-vim.cmd.colorscheme("kanagawa-dragon")
+-- vim.cmd.colorscheme("kanagawa-dragon") -- try wave also
+vim.cmd.colorscheme("catppuccin-mocha")
+
+
